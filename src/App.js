f0bd7873e1b5bc7bb248696bbbceff50aafb7636
Colby Tomita <cotomita808@gmail.com>
2024-04-29 16:53:06 -0700

    added a title for the app

--- src/App.js
+++ src/App.js
@@ -11,40 +11,45 @@ import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className="app-body">
-      <Navbar />
-      <section id="home">
-        <div className="spacer" id="space-1"></div>
-        <HeroSection />
-      </section>
-      <section id="about-me">
-        <div className="spacer" id="space-2"></div>
-        <About />
-      </section>
-      <section id="projects">
-        <div className="spacer" id="space-3"></div>
-        <Projects />
-      </section>
-      <section id="strengths">
-        <div className="spacer" id="space-4"></div>
-        <Strengths />
-      </section>
-      <section id="resume">
-        <div className="spacer" id="space-5"></div>
-        <Resume />
-      </section>
-      <section id="testimonials">
-        <div className="spacer" id="space-6"></div>
-        <Testimonials />
-      </section>
-      <section id="contact-me">
-        <div className="spacer" id="space-7"></div>
-        <Contact />
-      </section>
-      <div className="footer">
-        <p>© 2024 Colby Tomita</p>
+    <>
+      <head>
+        <title>Colby Tomita's Portfolio</title>
+      </head>
+      <div className="app-body">
+        <Navbar />
+        <section id="home">
+          <div className="spacer" id="space-1"></div>
+          <HeroSection />
+        </section>
+        <section id="about-me">
+          <div className="spacer" id="space-2"></div>
+          <About />
+        </section>
+        <section id="projects">
+          <div className="spacer" id="space-3"></div>
+          <Projects />
+        </section>
+        <section id="strengths">
+          <div className="spacer" id="space-4"></div>
+          <Strengths />
+        </section>
+        <section id="resume">
+          <div className="spacer" id="space-5"></div>
+          <Resume />
+        </section>
+        <section id="testimonials">
+          <div className="spacer" id="space-6"></div>
+          <Testimonials />
+        </section>
+        <section id="contact-me">
+          <div className="spacer" id="space-7"></div>
+          <Contact />
+        </section>
+        <div className="footer">
+          <p>© 2024 Colby Tomita</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
